83c687a427ab87899156fb48c4189bf37609f306
abera87 <57747453+abera87@users.noreply.github.com>
2019-12-19 12:25:45 +0530

    change in home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'StaticWebNGMyStore';
+  title = 'Static Web NG My-Store';
 }
