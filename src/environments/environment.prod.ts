4b977500c7f9aa53f960beaa187e3d94b10e2caf
abera87 <57747453+abera87@users.noreply.github.com>
2020-01-03 15:14:26 +0530

    remove constant from ENV

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 export const environment = {
-  production: true,
-  tenant: '207fad3e-ddcc-4e76-ac3d-001fcae96b28',
-  clientId: 'b8b7387f-1bf2-444c-b19b-333a00d701c6',
-  extraQueryParameter: 'nux=1', // This is optional
-  endpoints: {
-    "http://localhost:4200/api": 'b8b7387f-1bf2-444c-b19b-333a00d701c6', // Note, this is an object, you could add several things here
-    "https://staticwebapi.azurewebsites.net/WeatherForecast":"9f191287-dd57-4c61-8394-a4a3ea3d3c1a"
+  production: true
+  // tenant: '207fad3e-ddcc-4e76-ac3d-001fcae96b28',
+  // clientId: 'b8b7387f-1bf2-444c-b19b-333a00d701c6',
+  // extraQueryParameter: 'nux=1', // This is optional
+  // endpoints: {
+  //   "http://localhost:4200/api": 'b8b7387f-1bf2-444c-b19b-333a00d701c6', // Note, this is an object, you could add several things here
+  //   "https://staticwebapi.azurewebsites.net/WeatherForecast":"9f191287-dd57-4c61-8394-a4a3ea3d3c1a"
 }
 };
